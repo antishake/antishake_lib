e0a91a73d6ac99a97fec6323b8f38bc01aae4946
Ruraj Joshi <ruraj.joshi@rts.com.np>
2017-02-23 00:11:37 -0600

    Added dummy methods and interface for app dev

--- src/main/java/io/github/antishake/AntiShake.java
+++ src/main/java/io/github/antishake/AntiShake.java
@@ -1,7 +1,23 @@
 package io.github.antishake;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * Created by ruraj on 2/19/17.
  */
 public class AntiShake {
+
+  private AntiShake(MotionCorrectionListener listener) {
+
+  }
+
+  /**
+   * Calculates the next motion correction using accelerometer reading
+   * @param x
+   * @param y
+   * @param z
+   */
+  public void calculateTranslationVector(float x, float y, float z) {
+    throw new NotImplementedException();
+  }
 }
