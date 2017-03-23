6a5a6e98b1ce00420518c0e3dd6dbfdb5086ad5f
Geoffrey <geo.geofe@gmail.com>
2017-03-22 19:40:07 -0500

    changed the data type from double to co-ordinates for circularbuffer

--- src/main/java/io/github/antishake/CircularBuffer.java
+++ src/main/java/io/github/antishake/CircularBuffer.java
@@ -1,5 +1,4 @@
 package io.github.antishake;
-import java.util.Arrays;
 
 /**
  * Created by Geofe on 3/22/17.
@@ -11,43 +10,48 @@ import java.util.Arrays;
         -will maintain write pointer when updating with the latest values
  */
 public class CircularBuffer {
-    public int read_pointer,write_pointer=0;
-    public int size_buffer =201;
-    double[] circularbuffer =new double[size_buffer];
-    boolean buffer_full_once=false;
-    public boolean isEmpty(){
-        return read_pointer==write_pointer;
+  private int readPointer, writePointer = 0;
+  private int sizeBuffer = 201;
+  private Coordinate[] circularbuffer = new Coordinate[sizeBuffer];
+  private boolean bufferFullOnce = false;
+
+  public boolean isEmpty() {
+    return readPointer == writePointer;
+  }
+
+  public void add(Coordinate element) {
+    if (readPointer < sizeBuffer) {
+      if (!bufferFullOnce) {
+        circularbuffer[readPointer] = element;
+        readPointer++;
+      }
+      if (bufferFullOnce) {
+        circularbuffer[readPointer] = element;
+        readPointer++;
+        writePointer++;
+      }
+    } else {
+      readPointer = 0;
+      circularbuffer[readPointer] = element;
+      readPointer++;
+      bufferFullOnce = true;
     }
-    public void add(double element){
-        if(read_pointer<size_buffer){
-            if(!buffer_full_once){
-                circularbuffer[read_pointer]=element;
-                read_pointer++;
-            }
-            if(buffer_full_once){
-                circularbuffer[read_pointer]=element;
-                read_pointer++;
-                write_pointer++;
-            }
+  }
 
-        }
-        else{
-            read_pointer=0;
-            circularbuffer[read_pointer]=element;
-            read_pointer++;
-            buffer_full_once=true;
-
-        }
-
+  public Coordinate[] get_elements() {
+    if (!isEmpty()) {
+      return circularbuffer;
     }
-    public double[] get_elements(){
-        if(!isEmpty()){
-            return circularbuffer;
-        }
-        return circularbuffer;
+    return circularbuffer;
 
-    }
+  }
 
+  public int getReadPointer() {
+    return readPointer;
+  }
 
+  public int getWritePointer() {
+    return writePointer;
+  }
 }
 
