c63dd839fcdd7d622098bf228c7ba606a131bfb4
Ruraj Joshi <ruraj.joshi@rts.com.np>
2017-04-09 14:49:33 -0500

    #68 Implemented a background thread. Might want to merge before #84 is implemented.

--- src/main/java/io/github/antishake/utils/ConfigEnum.java
+++ src/main/java/io/github/antishake/utils/ConfigEnum.java
@@ -10,8 +10,8 @@ public enum ConfigEnum {
   SPRING_CONSTANT(10),
   CIRCULAR_BUFFER_IN_SEC(4),  // Number of seconds that the circular buffer should have the latest data
   SAMPLING_RATE_IN_HZ(50),  // Sampling rate to get the accelerometer data
-  SHAKE_DETECTION_THRESHOLD(46),  // Threshold to compare for shake detection
-  SHAKE_DETECTION_CHECK_TIME_IN_SEC(1.8),  // To check the shake detection, number of seconds that the latest data should be considered from circular buffer
+  SHAKE_DETECTION_THRESHOLD(45),  // Threshold to compare for shake detection
+  SHAKE_DETECTION_CHECK_TIME_IN_SEC(1.0),  // To check the shake detection, number of seconds that the latest data should be considered from circular buffer
   TUNE_CONVOLVE_OUTPUT(2);  // To tune the convolution output
 
   private final double value;
